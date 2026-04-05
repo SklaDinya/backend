8e1a835347ac003f403e10a93e03824b0abf4bc6
Simonovich Roman <srd2424@yandex.ru>
2026-04-05 18:31:45 +0300

    fix: replace jackson to gson

--- src/persistence-postgres/build.gradle.kts
+++ src/persistence-postgres/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     implementation(project(":domain"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+    implementation("com.google.code.gson:gson:2.10.1")
     runtimeOnly("org.postgresql:postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
