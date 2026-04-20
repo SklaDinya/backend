13825f7bc029137dbcb4d682c323d9df1f1a0100
Simonovich Roman <srd2424@yandex.ru>
2026-04-20 23:26:15 +0300

    fix: test configuration

--- src/tests-unit/src/test/java/skladinya/persistence/repositories/TestApplication.java
+++ src/tests-unit/src/test/java/skladinya/persistence/repositories/TestApplication.java
@@ -6,5 +6,5 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-@EntityScan("skladinya.persistence.entities")
+@EntityScan("skladinya.persistence")
 public class TestApplication {}
