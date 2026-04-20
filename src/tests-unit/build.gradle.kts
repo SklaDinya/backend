45844c76e270e018f9ec90cfcacd73c93ae57dc6
Simonovich Roman <srd2424@yandex.ru>
2026-04-20 23:42:51 +0300

    fix: restructure tests (unit -> integration)

--- src/tests-unit/build.gradle.kts
+++ src/tests-unit/build.gradle.kts
@@ -1,11 +1,5 @@
 plugins {
     id("java-library")
-    id("org.springframework.boot") version "3.3.4"
-    id("io.spring.dependency-management") version "1.1.6"
-}
-
-repositories {
-    mavenCentral()
 }
 
 description = "tests-unit"
@@ -13,18 +7,17 @@ group = "skladinya.tests.unit"
 
 dependencies {
     implementation(project(":domain"))
-    implementation(project(":persistence-postgres"))
     implementation(project(":tests-helper"))
+    // Add project dependencies here
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    testImplementation("com.h2database:h2")
+    // Add other dependencies here
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+// Add unit tests to the src/test/java in that module to package skladinya.tests.unit.<rest.path.to.tested.class.package>
