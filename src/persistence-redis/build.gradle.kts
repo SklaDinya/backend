34b5c0ef0f65d027e57fcc5ead79111c3b559896
Nikolay Baranov <nikolay.baranov.2003@yandex.ru>
2026-04-24 08:33:28 +0300

    fix: spring version location

--- src/persistence-redis/build.gradle.kts
+++ src/persistence-redis/build.gradle.kts
@@ -3,8 +3,6 @@ import java.util.Date
 
 plugins {
     id("java-library")
-    id("org.springframework.boot") version "3.5.14"
-    id("io.spring.dependency-management") version "1.1.7"
 }
 
 description = "persistence-redis"
