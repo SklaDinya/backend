2bc0793ac845d79d11c6ba2c0929454f370ed0b0
Nikolay Baranov <nikolay.baranov.2003@yandex.ru>
2026-04-30 15:19:52 +0300

    fix: docker compose windows

--- deploy/dev/deploy.bat
+++ deploy/dev/deploy.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 REM End previous session
-docker compose -f docker-compose.yaml down
+docker compose -f docker-compose-win.yaml down
 if errorlevel 1 exit /b %errorlevel%
 
 REM Redis - no actions to prepare
@@ -20,5 +20,5 @@ call spring\copy_files.bat
 if errorlevel 1 exit /b %errorlevel%
 
 REM Start
-docker compose -f docker-compose.yaml up -d
+docker compose -f docker-compose-win.yaml up -d
 if errorlevel 1 exit /b %errorlevel%
