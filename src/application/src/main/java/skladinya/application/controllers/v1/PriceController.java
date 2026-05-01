43c7f4f1bf17d9ed3db25b66343b7c95a1b7cb4b
Simonovich Roman <srd2424@yandex.ru>
2026-05-01 13:26:17 +0300

    feat: init booking controllers

---
+++ src/application/src/main/java/skladinya/application/controllers/v1/PriceController.java
@@ -0,0 +1,9 @@
+package skladinya.application.controllers.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/storages")
+public class PriceController {
+}
