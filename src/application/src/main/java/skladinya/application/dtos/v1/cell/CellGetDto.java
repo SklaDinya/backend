1a9d610e264775dde28da82ef9845ed365f32bc8
Nikolay Baranov <nikolay.baranov.2003@yandex.ru>
2026-05-01 20:27:02 +0300

    feat: cell controller

---
+++ src/application/src/main/java/skladinya/application/dtos/v1/cell/CellGetDto.java
@@ -0,0 +1,36 @@
+package skladinya.application.dtos.v1.cell;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CellGetDto {
+
+    @NotNull
+    private UUID id;
+
+    @NotNull
+    private UUID storageId;
+
+    @NotBlank
+    @Size(min = 1, max = 255)
+    private String name;
+
+    @NotBlank
+    @Size(min = 1, max = 255)
+    private String cellClass;
+
+    @NotNull
+    private OffsetDateTime createdAt;
+}
