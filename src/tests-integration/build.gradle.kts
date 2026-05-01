ab7f22e7b2518e657bffe081f8553e8253aa8ee5
Nikolay Baranov <nikolay.baranov.2003@yandex.ru>
2026-05-01 17:16:39 +0300

    feat: email service tests

--- src/tests-integration/build.gradle.kts
+++ src/tests-integration/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(project(":domain"))
     implementation(project(":persistence-postgres"))
     implementation(project(":persistence-redis"))
+    implementation(project(":services-email"))
     implementation(project(":tests-helper"))
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
@@ -24,9 +25,19 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-data-redis")
+    testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     testImplementation("com.h2database:h2")
+    testImplementation("com.icegreen:greenmail-junit5:2.1.8")
+}
+
+val copyEmailTemplates by tasks.registering(Copy::class) {
+    from(project(":services-email").sourceSets["main"].resources + "/templates")
+    include("*.html")
+    into("src/test/resources/templates")
+    dependsOn(tasks.processTestResources)
 }
 
 tasks.test {
     useJUnitPlatform()
+    dependsOn(copyEmailTemplates)
 }
