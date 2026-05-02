b1383eac47234464386b7e7fcac802c3d028a695
Simonovich Roman <srd2424@yandex.ru>
2026-05-02 22:53:42 +0300

    feat: implement booking & payment controllers

--- src/application/src/main/java/skladinya/application/controllers/v1/PaymentController.java
+++ src/application/src/main/java/skladinya/application/controllers/v1/PaymentController.java
@@ -1,9 +1,41 @@
 package skladinya.application.controllers.v1;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+import skladinya.application.converters.v1.booking.BookingGetUserDtoConverter;
+import skladinya.application.dtos.v1.booking.BookingGetUserDto;
+import skladinya.application.dtos.v1.payment.PaymentNoOpDto;
+import skladinya.application.dtos.v1.payment.PaymentRandomDto;
+import skladinya.application.security.RoleChecker;
+import skladinya.domain.services.BookingService;
 
 @RestController
 @RequestMapping("/api/v1/payments")
 public class PaymentController {
+
+    private final BookingService bookingService;
+    private final RoleChecker roleChecker;
+
+    public PaymentController(BookingService bookingService, RoleChecker roleChecker) {
+        this.bookingService = bookingService;
+        this.roleChecker = roleChecker;
+    }
+
+    @PostMapping("/noop")
+    public BookingGetUserDto payNoOp(
+            @RequestHeader String authorization,
+            @Valid @RequestBody PaymentNoOpDto dto) {
+        var data = roleChecker.requireClient(authorization);
+        var result = bookingService.payNoOp(data.userId(), dto.getReceipt());
+        return BookingGetUserDtoConverter.toDto(result);
+    }
+
+    @PostMapping("/random")
+    public BookingGetUserDto payRandom(
+            @RequestHeader String authorization,
+            @Valid @RequestBody PaymentRandomDto dto) {
+        var data = roleChecker.requireClient(authorization);
+        var result = bookingService.payRandom(data.userId(), dto.getReceipt());
+        return BookingGetUserDtoConverter.toDto(result);
+    }
 }
