219a6e3d4b7e8998f9fe6f6e8a9f4f9df52c3e7c
Simonovich Roman <srd2424@yandex.ru>
2026-05-02 14:44:50 +0300

    feat: implement price service

--- src/services-price/src/main/java/skladinya/services/price/PriceServiceImpl.java
+++ src/services-price/src/main/java/skladinya/services/price/PriceServiceImpl.java
@@ -2,23 +2,70 @@ package skladinya.services.price;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.RequestScope;
+import skladinya.domain.exceptions.SklaDinyaException;
+import skladinya.domain.helpers.Synchronizer;
 import skladinya.domain.models.price.Price;
 import skladinya.domain.models.price.PriceCreate;
+import skladinya.domain.repositories.PriceRepository;
 import skladinya.domain.services.PriceService;
+import skladinya.domain.services.StorageService;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequestScope
 public class PriceServiceImpl implements PriceService {
+
+    private final PriceRepository priceRepository;
+    private final StorageService storageService;
+    private final Synchronizer synchronizer;
+
+    public PriceServiceImpl(
+            PriceRepository priceRepository,
+            StorageService storageService,
+            Synchronizer synchronizer
+    ) {
+        this.priceRepository = priceRepository;
+        this.storageService = storageService;
+        this.synchronizer = synchronizer;
+    }
+
     @Override
     public Price create(UUID storageId, PriceCreate createForm) {
-        return null;
+        return synchronizer.executeTransactionFunction(() -> {
+
+            try {
+                storageService.getByStorageId(storageId);
+            } catch (SklaDinyaException ex) {
+                throw SklaDinyaException.notFound("Storage not found");
+            }
+
+            Price price = new Price(
+                    UUID.randomUUID(),
+                    storageId,
+                    createForm.cellClass(),
+                    createForm.price(),
+                    LocalDateTime.now()
+            );
+
+            return priceRepository.create(price);
+        });
     }
 
     @Override
     public List<Price> getAllByStorageId(UUID storageId) {
-        return List.of();
+        return synchronizer.executeTransactionFunction(() -> {
+
+            try {
+                storageService.getByStorageId(storageId);
+            } catch (SklaDinyaException ex) {
+                throw SklaDinyaException.notFound("Storage not found");
+            }
+
+            return priceRepository.getAllByStorageId(storageId);
+        });
     }
+
 }
