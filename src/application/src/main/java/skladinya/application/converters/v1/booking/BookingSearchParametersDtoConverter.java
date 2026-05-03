5f2ede09debb7551bc653559479e36e08d25c16f
Nikolay Baranov <nikolay.baranov.2003@yandex.ru>
2026-05-03 19:19:27 +0300

    fix: bookings

--- src/application/src/main/java/skladinya/application/converters/v1/booking/BookingSearchParametersDtoConverter.java
+++ src/application/src/main/java/skladinya/application/converters/v1/booking/BookingSearchParametersDtoConverter.java
@@ -4,6 +4,8 @@ import skladinya.application.converters.v1.help.TimeConverter;
 import skladinya.application.dtos.v1.booking.BookingSearchParametersDto;
 import skladinya.domain.models.booking.BookingSearchOptions;
 
+import java.util.List;
+
 
 public class BookingSearchParametersDtoConverter {
 
@@ -14,7 +16,9 @@ public class BookingSearchParametersDtoConverter {
         return new BookingSearchOptions(
                 TimeConverter.toLocalDateTime(dto.getStartBooking()),
                 TimeConverter.toLocalDateTime(dto.getEndBooking()),
-                dto.getStatuses().stream().map(BookingStatusDtoConverter::toCoreEntity).toList(),
+                dto.getStatuses() == null
+                        ? List.of()
+                        : dto.getStatuses().stream().map(BookingStatusDtoConverter::toCoreEntity).toList(),
                 dto.getPageSize(),
                 dto.getPageNumber()
         );
